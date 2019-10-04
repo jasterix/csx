38899ec387d6a95a37b90a27efa612c44d39c218
Jasterix <ljcalixte@gmail.com>
2019-10-04 15:34:17 -0400

    prompt for after

---
+++ Closure,Scope,Execution/after.js
@@ -0,0 +1,2 @@
+Challenge: after
+Write a function after that takes the number of times the callback needs to be called before being executed as the first parameter and the callback as the second parameter.
--- Closure,Scope,Execution/saveOutput.js
+++ Closure,Scope,Execution/saveOutput.js
@@ -13,7 +13,7 @@ const saveOutput = (callback, password) => {
 
 // Uncomment these to check your work!
 const multiplyBy2 = function(num) { return num * 2; };
-const multBy2AndLog = saveOutput(multiplyBy2, 'boo');
+const multBy2AndLog = savsaeOutput(multiplyBy2, 'boo');
 console.log(multBy2AndLog(2)); // should log: 4
 console.log(multBy2AndLog(9)); // should log: 18
 console.log(multBy2AndLog('boo')); // should log: { 2: 4, 9: 18 }

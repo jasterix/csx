ceb43f94f1d355bfbadce17a77f835eca609a4f3
Jasterix <ljcalixte@gmail.com>
2019-10-04 15:33:06 -0400

    solution for saveOutput

--- Closure,Scope,Execution/saveOutput.js
+++ Closure,Scope,Execution/saveOutput.js
@@ -1,1 +1,25 @@
 // Ceate a function saveOutput that accepts a function (that will accept one argument), and a string (that will act as a password). saveOutput will then return a function that behaves exactly like the passed-in function, except for when the password string is passed in as an argument. When this happens, the returned function will return an object with all previously passed-in arguments as keys, and the corresponding outputs as values
+
+const saveOutput = (callback, password) => {
+  let output = {}
+  const inner = (arg) => {
+    if (arg === password){
+      return output
+    }
+    return output[arg] = callback(arg)
+  }
+  return inner
+}
+
+// Uncomment these to check your work!
+const multiplyBy2 = function(num) { return num * 2; };
+const multBy2AndLog = saveOutput(multiplyBy2, 'boo');
+console.log(multBy2AndLog(2)); // should log: 4
+console.log(multBy2AndLog(9)); // should log: 18
+console.log(multBy2AndLog('boo')); // should log: { 2: 4, 9: 18 }
+
+// Test
+// Is it working? Check my answer
+// saveOutput should create and return a function
+// The function returned from saveOutput should behave exactly like the passed-in function when not called with the password string
+// The function returned from saveOutput should return an object with all previously passed-in arguments and outputs when called with the password string
